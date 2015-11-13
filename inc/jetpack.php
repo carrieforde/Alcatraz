15b776e74129008e15c56cc06e90e5cbff94b6ad
carrieforde <carrie@carrieforde.com>
2015-11-13 10:02:16 -0800

    Initial commit of the Alcatraz theme

---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.me/
+ *
+ * @package alcatraz
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.me/support/infinite-scroll/
+ * See: https://jetpack.me/support/responsive-videos/
+ */
+function alcatraz_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'alcatraz_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+} // end function alcatraz_jetpack_setup
+add_action( 'after_setup_theme', 'alcatraz_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function alcatraz_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+		    get_template_part( 'template-parts/content', 'search' );
+		else :
+		    get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+} // end function alcatraz_infinite_scroll_render
