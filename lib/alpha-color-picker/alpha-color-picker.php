3e76381da4e9f5468ea15b6b2763a0a6023b44fa
jgonzo127 <jgonzo127@gmail.com>
2015-12-26 21:27:40 -0800

    Add alpha color picker

---
+++ lib/alpha-color-picker/alpha-color-picker.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Alpha Color Picker Customizer Control
+ *
+ * This control adds a second slider for opacity to the stock WordPress color picker,
+ * and it includes logic to seamlessly convert between RGBa and Hex color values as
+ * opacity is added to or removed from a color.
+ *
+ * This Alpha Color Picker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this Alpha Color Picker. If not, see <http://www.gnu.org/licenses/>.
+ */
+class Alcatraz_Customize_Alpha_Color_Control extends WP_Customize_Control {
+
+	/**
+	 * Official control name.
+	 */
+	public $type = 'alpha-color';
+
+	/**
+	 * Add support for palettes to be passed in.
+	 *
+	 * Supported palette values are true, false, or an array of RGBa and Hex colors.
+	 */
+	public $palette;
+
+	/**
+	 * Add support for showing the opacity value on the slider handle.
+	 */
+	public $show_opacity;
+
+	/**
+	 * Enqueue scripts and styles.
+	 */
+	public function enqueue() {
+		wp_enqueue_script(
+			'alpha-color-picker',
+			ALCATRAZ_URL . 'lib/alpha-color-picker/alpha-color-picker.js',
+			array( 'jquery', 'wp-color-picker' ),
+			ALCATRAZ_VERSION,
+			true
+		);
+		wp_enqueue_style(
+			'alpha-color-picker',
+			ALCATRAZ_URL . 'lib/alpha-color-picker/alpha-color-picker.css',
+			array( 'wp-color-picker' ),
+			ALCATRAZ_VERSION
+		);
+	}
+
+	/**
+	 * Render the control.
+	 */
+	public function render_content() {
+
+		// Process the palette
+		if ( is_array( $this->palette ) ) {
+			$palette = implode( '|', $this->palette );
+		} else {
+			// Default to true.
+			$palette = ( false === $this->palette || 'false' === $this->palette ) ? 'false' : 'true';
+		}
+
+		// Support passing show_opacity as string or boolean. Default to true.
+		$show_opacity = ( false === $this->show_opacity || 'false' === $this->show_opacity ) ? 'false' : 'true';
+
+		// Begin the output. ?>
+		<label>
+			<?php // Output the label and description if they were passed in.
+			if ( ! empty( $this->label ) ) {
+				echo '<span class="customize-control-title">' . esc_html( $this->label ) . '</span>';
+			}
+			if ( ! empty( $this->description ) ) {
+				echo '<span class="description customize-control-description">' . wp_kses_post( $this->description ) . '</span>';
+			} ?>
+			<input class="alpha-color-control" type="text" data-show-opacity="<?php echo $show_opacity; ?>" data-palette="<?php echo esc_attr( $palette ); ?>" data-default-color="<?php echo esc_attr( $this->settings['default']->default ); ?>" <?php $this->link(); ?>  />
+		</label>
+		<?php
+	}
+}
