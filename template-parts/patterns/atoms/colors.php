87e91cc48db6cc27d38e4702756ae79d2b1cd63e
carrieforde <carrie@carrieforde.com>
2016-11-02 18:09:55 -0700

    Set up color utility classes, mixin, and switch to using those in our color atom

--- template-parts/patterns/atoms/colors.php
+++ template-parts/patterns/atoms/colors.php
@@ -15,7 +15,7 @@ $colors = alcatraz_set_theme_colors();
 
 		<div class="color-chip">
 
-			<div class="color" style="background-color: <?php esc_attr_e( $value ); ?>"></div>
+			<div class="color <?php esc_attr_e( $key ); ?>-background"></div>
 			<span class="color-name">$<?php esc_html_e( $key ); ?> : <?php esc_html_e( $value ); ?></span>
 
 		</div>
