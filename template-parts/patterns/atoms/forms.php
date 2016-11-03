57496a4e84fe631d7eda4d676c0790fd3ac95e46
carrieforde <carrie@carrieforde.com>
2016-11-03 15:57:30 -0700

    Clean up and add a few more of our form elements

--- template-parts/patterns/atoms/forms.php
+++ template-parts/patterns/atoms/forms.php
@@ -9,36 +9,38 @@
 	<h2 class="section-heading"><?php esc_html_e( 'Forms', 'alcatraz' ); ?></h2>
 
 	<div class="wrap">
-
 		<h3 class="section-sub-heading"><?php esc_html_e( 'Text Field', 'alcatraz' ); ?></h3>
-
 		<?php alcatraz_form_elements( array( 'tag' => 'text' ) ); ?>
-
 	</div>
 
 	<div class="wrap">
-
 		<h3 class="section-sub-heading"><?php esc_html_e( 'Textarea', 'alcatraz' ); ?></h3>
-
 		<?php alcatraz_form_elements( array( 'tag' => 'textarea' ) ); ?>
-
 	</div>
 
 	<div class="wrap">
-
 		<h3 class="section-sub-heading"><?php esc_html_e( 'Select', 'alcatraz' ); ?></h3>
-
 		<?php alcatraz_form_elements( array( 'tag' => 'select' ) ); ?>
-
 	</div>
 
-	<?php
+	<div class="wrap">
+		<h3 class="section-sub-heading"><?php esc_html_e( 'Text Input', 'alcatraz' ); ?></h3>
+		<?php
 		alcatraz_form_elements( array(
-				'type'          => 'email',
-				'placeholder'   => 'email@example.com',
-				'autocomplete'  => 'on',
-			)
-		);
-	?>
+			'type'          => 'text',
+			'placeholder'   => 'Text Input',
+		) );
+		?>
+	</div>
+
+	<div class="wrap">
+		<h3 class="section-sub-heading"><?php esc_html_e( 'Radio', 'alcatraz' ); ?></h3>
+		<?php alcatraz_form_elements( array( 'type' => 'radio' ) ); ?>
+	</div>
+
+	<div class="wrap">
+		<h3 class="section-sub-heading"><?php esc_html_e( 'Checkbox', 'alcatraz' ); ?></h3>
+		<?php alcatraz_form_elements( array( 'type' => 'checkbox' ) ); ?>
+	</div>
 
 </section>
