fa4b4cfa18212e16ad0762240e59cde809604b15
carrieforde <carrie@carrieforde.com>
2016-11-04 11:05:10 -0700

    Fix a typo and make sure all of our typography patterns are displayed

--- inc/patterns/atoms.php
+++ inc/patterns/atoms.php
@@ -340,7 +340,7 @@ function alcatraz_typography( $args = array() ) {
 
 			<?php break;
 
-		case 'parapgraph' : ?>
+		case 'paragraph' : ?>
 
 			<p><?php esc_html_e( 'A paragraph (from the Greek paragraphos, "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.', 'alcatraz' ); ?></p>
 
--- template-parts/patterns/atoms/typography.php
+++ template-parts/patterns/atoms/typography.php
@@ -24,4 +24,37 @@
 		<?php alcatraz_typography( array( 'element' => 'headings' ) ); ?>
 	</div>
 
+	<div class="wrap">
+		<?php alcatraz_pattern_doc( array(
+			'heading'      => 'Horizontal Rule',
+			'output'       => '<hr />',
+		) ); ?>
+
+		<?php alcatraz_typography( array( 'element' => 'hr' ) ); ?>
+	</div>
+
+	<div class="wrap">
+		<?php alcatraz_pattern_doc( array(
+			'heading'      => 'Inline Elements',
+		) ); ?>
+
+		<?php alcatraz_typography( array( 'element' => 'inline' ) ); ?>
+	</div>
+
+	<div class="wrap">
+		<?php alcatraz_pattern_doc( array(
+			'heading'      => 'Paragraph',
+		) ); ?>
+
+		<?php alcatraz_typography( array( 'element' => 'paragraph' ) ); ?>
+	</div>
+
+	<div class="wrap">
+		<?php alcatraz_pattern_doc( array(
+			'heading'      => 'Preformatted',
+		) ); ?>
+
+		<?php alcatraz_typography( array( 'element' => 'preformatted' ) ); ?>
+	</div>
+
 </section>
