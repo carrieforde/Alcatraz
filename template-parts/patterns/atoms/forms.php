d652dfe3ae490f42f90255df6e7d45e1ef24c8eb
carrieforde <carrie@carrieforde.com>
2016-11-03 17:51:43 -0700

    Reorder some of the form element output

--- template-parts/patterns/atoms/forms.php
+++ template-parts/patterns/atoms/forms.php
@@ -9,21 +9,6 @@
 	<h2 class="section-heading"><?php esc_html_e( 'Forms', 'alcatraz' ); ?></h2>
 
 	<div class="wrap">
-		<h3 class="section-sub-heading"><?php esc_html_e( 'Text Field', 'alcatraz' ); ?></h3>
-		<?php alcatraz_form_elements( array( 'tag' => 'text' ) ); ?>
-	</div>
-
-	<div class="wrap">
-		<h3 class="section-sub-heading"><?php esc_html_e( 'Textarea', 'alcatraz' ); ?></h3>
-		<?php alcatraz_form_elements( array( 'tag' => 'textarea' ) ); ?>
-	</div>
-
-	<div class="wrap">
-		<h3 class="section-sub-heading"><?php esc_html_e( 'Select', 'alcatraz' ); ?></h3>
-		<?php alcatraz_form_elements( array( 'tag' => 'select' ) ); ?>
-	</div>
-
-	<div class="wrap">
 		<h3 class="section-sub-heading"><?php esc_html_e( 'Text Input', 'alcatraz' ); ?></h3>
 		<?php
 		alcatraz_form_elements( array(
@@ -41,6 +26,16 @@
 	<div class="wrap">
 		<h3 class="section-sub-heading"><?php esc_html_e( 'Checkbox', 'alcatraz' ); ?></h3>
 		<?php alcatraz_form_elements( array( 'type' => 'checkbox' ) ); ?>
+	</div>
+
+	<div class="wrap">
+		<h3 class="section-sub-heading"><?php esc_html_e( 'Textarea', 'alcatraz' ); ?></h3>
+		<?php alcatraz_form_elements( array( 'tag' => 'textarea' ) ); ?>
+	</div>
+
+	<div class="wrap">
+		<h3 class="section-sub-heading"><?php esc_html_e( 'Select', 'alcatraz' ); ?></h3>
+		<?php alcatraz_form_elements( array( 'tag' => 'select' ) ); ?>
 	</div>
 
 </section>
