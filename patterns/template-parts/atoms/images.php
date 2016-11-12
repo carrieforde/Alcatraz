6d1c4d91976cf87a85b51e8c10c8192bfb2709dd
carrieforde <carrie@carrieforde.com>
2016-11-11 19:38:49 -0800

    Add an doc for the post thumbnail

--- patterns/template-parts/atoms/images.php
+++ patterns/template-parts/atoms/images.php
@@ -10,10 +10,22 @@
 
 		<div class="wrap">
 			<?php alcatraz_pattern_doc( array(
-				'heading' => 'Card Image',
+				'heading'     => 'Post Thumbnail',
+				'description' => 'The default post thumbnail is 1200 x 740px',
+				'function'    => 'the_post_thumbnail()',
+				'output'      => alcatraz_image( array(
+					'use_img_url' => true,
+					'src' => 'https://unsplash.it/1200/740/?random',
+				) ),
+			) ); ?>
+		</div>
+
+		<div class="wrap">
+			<?php alcatraz_pattern_doc( array(
+				'heading'     => 'Card Image',
 				'description' => 'This is a 300 x 200px image.',
-				'function' => 'alcatraz_image( array( \'use_img_url\' => true ) )',
-				'output' => alcatraz_image( array( 'use_img_url' => true ) ),
+				'function'    => 'alcatraz_image( array( \'use_img_url\' => true ) )',
+				'output'      => alcatraz_image( array( 'use_img_url' => true ) ),
 			) ); ?>
 		</div>
 </section>
