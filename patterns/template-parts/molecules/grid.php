67a282202fcd46ca3c304d407fb5797f61a94bb6
carrieforde <carrie@carrieforde.com>
2016-11-12 17:23:56 -0800

    Add information about tweaking the grid settings

--- patterns/template-parts/molecules/grid.php
+++ patterns/template-parts/molecules/grid.php
@@ -12,7 +12,7 @@
 
 		<?php alcatraz_pattern_doc( array(
 			'heading'      => 'Grid',
-			'description'  => 'The Alcatraz Grid with gutters. Use Alcatraz\'s built-in grid classes to create layouts quickly. Grids are built using Bourbon and Neat, and are built on a 12-column system.',
+			'description'  => 'The Alcatraz Grid with gutters. Use Alcatraz\'s built-in grid classes to create layouts quickly. Grids are built using Bourbon and Neat, and are built on a 12-column system. The 12-column grid can be visualized or adjusted in <code>_grid-settings.scss</code>.',
 			'output'       => alcatraz_grid( array( 'gutter' => true ) ),
 		) ); ?>
 
@@ -22,7 +22,7 @@
 
 		<?php alcatraz_pattern_doc( array(
 			'heading'      => 'Grid without Gutter',
-			'description'  => 'The Alcatraz Grid without gutters. Add the <code>no-gutter</code> class to your column wrapper.',
+			'description'  => 'The Alcatraz Grid without gutters. Add the <code>no-gutter</code> class to your column wrapper (row).',
 			'output'       => alcatraz_grid( array( 'gutter' => false ) ),
 		) ); ?>
 
