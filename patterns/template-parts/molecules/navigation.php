7e11b7bc3370716c921c6d890a9c81bc664c1812
carrieforde <carrie@carrieforde.com>
2017-01-04 19:29:19 -0800

    Update social nav output call to alcatraz_get_social_nav

--- patterns/template-parts/molecules/navigation.php
+++ patterns/template-parts/molecules/navigation.php
@@ -12,7 +12,7 @@
 		'heading'      => 'Social Menu',
 		'description'  => 'Add a social menu in the Dashboard under Appearance > Menus. Create a new menu with links to your social media sites. Save the menu to the Social Menu location. You may show the menu in the footer by ticking the box in the Customizer, or show it any widget area with the Alcatraz Extras plugin.',
 		'function'     => 'alcatraz_social_nav()',
-		'output'       => alcatraz_social_nav(),
+		'output'       => alcatraz_get_social_nav(),
 	) ); ?>
 
 	<?php alcatraz_pattern_doc( array(
