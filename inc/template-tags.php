9b1f44e9d26a85a53aa308c1502fc50735b2c4d9
Carrie Forde <carrie@carrieforde.com>
2017-02-13 16:46:35 -0800

    For accessibility, we should just use CSS to hide the title, not eliminate it completely

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -164,6 +164,8 @@ function alcatraz_entry_title( $post_id = 0 ) {
 
 		if ( 'on' !== $hide_title ) {
 			$title = '<h1 class="entry-title">' . get_the_title( $post_id ) . '</h1>';
+		} else {
+			$title = '<h1 class="entry-title screen-reader-text">' . get_the_title( $post_id ) . '</h1>';
 		}
 
 	} else {
