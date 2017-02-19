4a3c4c83ab38dbea7b7b9e9dcec8a1adb0318ebb
Carrie Forde <carrie@carrieforde.com>
2017-02-19 10:30:20 -0800

    Update path to new Neat

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function( grunt ) {
 				sourceMap: true,
 				includePaths: [
 					'node_modules/bourbon/app/assets/stylesheets',
-					'node_modules/bourbon-neat/app/assets/stylesheets',
+					'node_modules/bourbon-neat/core/',
 					'node_modules/sanitize.scss'
 				]
 			},
