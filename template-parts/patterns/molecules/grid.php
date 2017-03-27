0df85b01be2fae84620df793ce475b4ae02d8dc3
Carrie Forde <carrie@carrieforde.com>
2017-03-26 17:16:31 -0700

    Update the grid descriptions

--- template-parts/patterns/molecules/grid.php
+++ template-parts/patterns/molecules/grid.php
@@ -10,13 +10,13 @@
 
 	<?php alcatraz_pattern_doc( array(
 		'heading'      => 'Grid',
-		'description'  => 'The Alcatraz Grid with gutters. Use Alcatraz\'s built-in grid classes to create layouts quickly. Grids are built using Bourbon and Neat, and are built on a 12-column system. The 12-column grid can be visualized or adjusted in <code>_grid-settings.scss</code>.',
+		'description'  => 'The Alcatraz Grid with gutters. Use Alcatraz\'s built-in grid classes to create layouts quickly. Grids are built using Bourbon and Neat, and are built on a 12-column system. The 12-column grid can be visualized by placing <code>@include grid-visual;</code> on the parent container, or adjusted in <code>_grid-settings.scss</code>.',
 		'output'       => alcatraz_grid( array( 'gutter' => true ) ),
 	) ); ?>
 
 	<?php alcatraz_pattern_doc( array(
 		'heading'      => 'Grid without Gutter',
-		'description'  => 'The Alcatraz Grid without gutters. Add the <code>no-gutter</code> class to your column wrapper (row).',
+		'description'  => 'The Alcatraz Grid without gutters. Add the <code>--no-gutter</code> modifier to your column class. E.g. <code>alcatraz-col--no-gutter--4</code>.',
 		'output'       => alcatraz_grid( array( 'gutter' => false ) ),
 	) ); ?>
 </section>
