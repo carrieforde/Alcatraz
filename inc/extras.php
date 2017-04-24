8910e101b28eabfc6af172e841b86a77f93a04e5
Carrie Forde <carrie@carrieforde.com>
2017-04-24 09:33:13 -0700

    Be sure to grab site options for adding body class for footer widgets

--- inc/extras.php
+++ inc/extras.php
@@ -18,6 +18,9 @@ add_filter( 'body_class', 'alcatraz_body_classes' );
  */
 function alcatraz_body_classes( $classes ) {
 
+	// Get options.
+	$options = get_option( 'alcatraz_options' );
+
 	// If primary sidebar is active, and we're not on a patterns page, add the 'has-sidebar' class.
 	if ( is_active_sidebar( 'primary-sidebar' ) && ! ( is_page_template( 'template-patterns-atoms.php' ) || is_page_template( 'template-patterns-molecules.php' ) || is_page_template( 'template-patterns-organisms.php' ) ) ) {
 		$classes[] = 'has-sidebar';
