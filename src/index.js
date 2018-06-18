7625a7b4bc65defaee2d265392488b0c91836f92
Carrie Forde <carrie@carrieforde.com>
2018-06-17 19:12:21 -0700

    Moves scripts for better organization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+/**
+ * Alcatraz Front End Styles & JS.
+ */
+
 // Import styles for compilation.
 import './sass/main';
 
 // Import front end scripts.
 // import '../vendor/jquery-mobile/jquery.mobile.custom';
-import './skip-link-focus-fix';
-import './init';
+import './scripts/skip-link-focus-fix';
+import './scripts/init';
