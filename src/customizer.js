35a86226b786e8596ebd43f5b171935f723e2c89
Carrie Forde <carrie@carrieforde.com>
2018-06-17 19:00:52 -0700

    Adds preview functionality for custom logo & hide tagline; refactors to use vanilla JS

--- src/customizer.js
+++ src/customizer.js
@@ -2,68 +2,74 @@
  * Alcatraz Customizer JS.
  */
 
-(function($) {
-  var $body = $('body');
-
+(function() {
   // Handle live previewing for the site title.
-  wp.customize('blogname', function(value) {
-    value.bind(function(to) {
-      $('.site-title a').text(to);
+  wp.customize('blogname', value => {
+    value.bind(
+      to => (document.querySelector('.site-title a').textContent = to)
+    );
+  });
+
+  // Hides / unhides site title if logo exists.
+  wp.customize('custom_logo', setting => {
+    setting.bind(logo => {
+      const siteTitle = document.querySelector('.site-title');
+
+      if (logo) {
+        siteTitle.classList.add('screen-reader-text');
+      }
+
+      if (!logo && siteTitle.classList.contains('screen-reader-text')) {
+        siteTitle.classList.remove('screen-reader-text');
+      }
     });
   });
 
   // Handle live previewing for the site description.
-  wp.customize('blogdescription', function(value) {
-    value.bind(function(to) {
-      $('.site-description').text(to);
-    });
+  wp.customize('blogdescription', value => {
+    value.bind(
+      to => (document.querySelector('.site-description').textContent = to)
+    );
   });
 
-  // Handle live previewing for the site layout.
-  wp.customize('alcatraz_options[site_layout]', function(value) {
-    value.bind(function(to) {
-      $body.removeClass('full-width boxed boxed-content');
-      $body.addClass(to);
+  // Hides / unhides site tagline.
+  wp.customize('alcatraz_options[hide_tagline]', setting => {
+    setting.bind(hide => {
+      const siteTagline = document.querySelector('.site-description');
+
+      if (hide) {
+        siteTagline.classList.add('screen-reader-text');
+      }
+
+      if (!hide && siteTagline.classList.contains('screen-reader-text')) {
+        siteTagline.classList.remove('screen-reader-text');
+      }
     });
   });
 
   // Handle live previewing for the header style.
-  wp.customize('alcatraz_options[header_style]', function(value) {
-    value.bind(function(to) {
-      $body.removeClass(
-        'header-style-default header-style-short header-style-side'
+  wp.customize('alcatraz_options[header_style]', value => {
+    value.bind(to => {
+      // Remove existing header class(es).
+      document.body.classList.remove(
+        'header-style-stacked',
+        'header-style-inline'
       );
-      $body.addClass('header-style-' + to);
-    });
-  });
 
-  // Handle live previewing for the mobile nav toggle style.
-  wp.customize('alcatraz_options[mobile_nav_toggle_style]', function(value) {
-    value.bind(function(to) {
-      $body.removeClass(
-        'mobile-nav-toggle-style-hamburger mobile-nav-toggle-style-button'
-      );
-      $body.addClass('mobile-nav-toggle-style-' + to);
+      document.body.classList.add(`header-style-${to}`);
     });
   });
 
   // Handle live previewing for the mobile nav style.
-  wp.customize('alcatraz_options[mobile_nav_style]', function(value) {
-    value.bind(function(to) {
-      $body.removeClass(
-        'mobile-nav-style-default mobile-nav-style-slide-left mobile-nav-style-slide-right mobile-nav-style-full-screen'
+  wp.customize('alcatraz_options[mobile_nav_style]', value => {
+    value.bind(to => {
+      // Remove existing mobile menu classes.
+      document.body.classList.remove(
+        'mobile-nav-style-slide-out',
+        'mobile-nav-style-full-screen'
       );
-      $body.addClass('mobile-nav-style-' + to);
-    });
-  });
 
-  // Handle live previewing for the mobile nav sub menu toggle style.
-  wp.customize('alcatraz_options[sub_menu_toggle_style]', function(value) {
-    value.bind(function(to) {
-      $body.removeClass(
-        'sub-menu-toggle-style-chevron sub-menu-toggle-style-plus-minus'
-      );
-      $body.addClass('sub-menu-toggle-style-' + to);
+      document.body.classList.add(`mobile-nav-style-${to}`);
     });
   });
-})(jQuery);
+})();
